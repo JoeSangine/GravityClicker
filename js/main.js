9ecfac49acc5dbf9b5d58e7215e1f42e76f975c6
Andy Wood <85075266+TheWoodenMan@users.noreply.github.com>
2022-06-30 23:30:01 +0100

    documentation
    I improved the documentation to make the code more readable.

--- js/main.js
+++ js/main.js
@@ -20,24 +20,30 @@ function gainOne() {
 }
 
 function roidClick() {
+  // first define the current size of the asteroid as a string
   roidHeight = document.querySelector("#asteroid").style.height;
+  // Then trim the "px" from the end to make it a string number
   roidGain = roidHeight.substring(0, roidHeight.length - 2);
+  // convert the string to a number then add it to mass.
   mass += Number(roidGain);
+  //  update black hole size
   document.querySelector("#gravity").innerHTML = mass;
   blackHoleBorder += 0.1;
   document.querySelector(
     "#gravity"
   ).style.border = `${blackHoleBorder}rem solid black`;
-
+  // call a random roll for objects as a result of the click
   randomObject();
+  // hide the asteroid again
   toggleHidden("#asteroid");
 }
 
 function randomObject() {
+  // randomise a number between 1 and 1000
   let randomChance = Math.floor(Math.random() * 1000);
 
   if (randomChance >= 1 && randomChance <= 50) {
-    // 5% chance or 1 in 20 clicks.
+    // 5% chance or 1 in 20 clicks to spawn a new asteroid.
     spawnAsteroid();
   }
 }
@@ -49,7 +55,7 @@ function spawnAsteroid() {
     Math.ceil(Math.random() * 50) * (Math.round(Math.random()) ? 1 : -1);
   let yRand =
     Math.ceil(Math.random() * 50) * (Math.round(Math.random()) ? 1 : -1);
-
+  // Randomise the size of the asteroid between 50 and 100 pixels.
   let sizeRand = Math.ceil(Math.random() * 50) + 50;
 
   // remove the "hidden" class from the asteroid and update it's position.
@@ -61,5 +67,6 @@ function spawnAsteroid() {
 }
 
 function toggleHidden(element) {
+  // Function to hide or unhide whatever element is passed into it's arguments.
   document.querySelector(`${element}`).classList.toggle("hidden");
 }
