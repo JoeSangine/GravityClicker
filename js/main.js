0dc14d62f02ab46e4d6aca324d417bad75e8165e
Andy Wood <85075266+TheWoodenMan@users.noreply.github.com>
2022-06-30 20:51:28 +0100

    black hole
    black-hole growth implemented.
    also refactored a little

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 // create a button that appears after gravity = 100 make that button have a function which gains 1 total in gravity every second repeat process for gravity = 1000 so on and so forth
-// 
+//
 
+let mass = 0;
+let blackHoleBorder = 0.1;
 
-let mass = 0
+document.querySelector("#gainMass").addEventListener("click", gainOne);
 
+function gainOne() {
+  mass += 1;
+  document.querySelector("#gravity").innerHTML = mass;
 
-
-document.querySelector('#gainMass').addEventListener('click',gainOne)
-
-function gainOne(){
-   mass = mass + 1
-    document.querySelector('#gravity').innerHTML = mass
-
+  blackHoleBorder += 0.1;
+  document.querySelector(
+    "#gravity"
+  ).style.border = `${blackHoleBorder}rem solid black`;
 }
