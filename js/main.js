81ac1bd87487120995f3674aac407bb43e523fad
Andy Wood <85075266+TheWoodenMan@users.noreply.github.com>
2022-07-02 16:31:28 +0100

    updated definitions
    updating definitions to improve readability

--- js/main.js
+++ js/main.js
@@ -5,11 +5,6 @@ document.querySelector("#gainMass").addEventListener("click", gainOne);
 document.querySelector("#asteroid").addEventListener("click", roidClick);
 document.querySelector("#gainMass").addEventListener("click", buttonAppear);
 
-
-
-
-
-
 function gainOne() {
   mass += 1;
   document.querySelector("#gravity").innerHTML = mass;
@@ -23,6 +18,8 @@ function gainOne() {
 }
 
 function roidClick() {
+  // Function to control the behaviour of the object and blackhole when the asteroid is clicked on.
+
   // first define the current size of the asteroid as a string
   roidHeight = document.querySelector("#asteroid").style.height;
   // Then trim the "px" from the end to make it a string number
@@ -42,6 +39,8 @@ function roidClick() {
 }
 
 function randomObject() {
+  // Function to give a random chance on click for an object to appear on the viewport
+
   // randomise a number between 1 and 1000
   let randomChance = Math.floor(Math.random() * 1000);
 
@@ -52,17 +51,18 @@ function randomObject() {
 }
 
 function spawnAsteroid() {
-  // Randomise the size from -50% to 50% of viewport
-  // Use the random values above to choose an x/y position for the asteroid
+  // Spawn a randomly sized asteroid at a random place on the viewport.
+
+  // Randomise the x/y position for the asteroid
   let xRand =
     Math.ceil(Math.random() * 50) * (Math.round(Math.random()) ? 1 : -1);
   let yRand =
     Math.ceil(Math.random() * 50) * (Math.round(Math.random()) ? 1 : -1);
+
   // Randomise the size of the asteroid between 50 and 100 pixels.
   let sizeRand = Math.ceil(Math.random() * 50) + 50;
 
   // remove the "hidden" class from the asteroid and update it's position.
-
   document.querySelector(
     "#asteroid"
   ).style = `height: ${sizeRand}px; margin-top: ${yRand}vh; margin-left: ${xRand}vw`;
@@ -70,52 +70,36 @@ function spawnAsteroid() {
 }
 
 function toggleHidden(element) {
-  // Function to hide or unhide whatever element is passed into it's arguments.
+  // Function to hide or unhide ANY element passed into it as an argument.
   document.querySelector(`${element}`).classList.toggle("hidden");
 }
 
-function buttonAppear(){
+function buttonAppear() {
   if (mass >= 10 && mass <= 100) {
-      document.querySelector('#b1').style.visibility = "visible";
-  
-  
+    document.querySelector("#b1").style.visibility = "visible";
+  } else if (mass >= 1000 && mass <= 10000) {
+    document.querySelector("#b2").style.visibility = "visible";
+  } else if (mass >= 10000 && mass <= 100000) {
+    document.querySelector("#b2").style.visibility = "visible";
+  } else if (mass >= 100000 && mass <= 1000000) {
+    document.querySelector("#b3").style.visibility = "visible";
+  } else if (mass >= 1000000 && mass <= 10000000) {
+    document.querySelector("#b4").style.visibility = "visible";
+  } else if (mass >= 10000000 && mass <= 100000000) {
+    document.querySelector("#b5").style.visibility = "visible";
+  } else if (mass >= 100000000 && mass <= 1000000000) {
+    document.querySelector("#b6").style.visibility = "visible";
+  } else if (mass >= 1000000000 && mass <= 10000000000) {
+    document.querySelector("#b7").style.visibility = "visible";
+  } else if (mass >= 10000000000 && mass <= 100000000000) {
+    document.querySelector("#b8").style.visibility = "visible";
+  } else if (mass >= 100000000000 && mass <= 1000000000000) {
+    document.querySelector("#b9").style.visibility = "visible";
+  } else if (mass >= 1000000000000 && mass <= 10000000000000) {
+    document.querySelector("#b10").style.visibility = "visible";
+  } else if (mass >= 10000000000000 && mass <= 100000000000000) {
+    document.querySelector("#b11").style.visibility = "visible";
+  } else if (mass >= 100000000000000 && mass <= 1000000000000000) {
+    document.querySelector("#b12").style.visibility = "visible";
   }
-
-  else if (mass >= 1000 && mass<=10000){
-      document.querySelector('#b2').style.visibility = "visible";
-  }
-  else if (mass >= 10000 && mass<=100000){
-      document.querySelector('#b2').style.visibility = "visible";
-  }
-    else if (mass >= 100000 && mass<=1000000){
-      document.querySelector('#b3').style.visibility = "visible";
-  }
-    else if (mass >= 1000000 && mass<=10000000){
-      document.querySelector('#b4').style.visibility = "visible";
-  }
-    else if (mass >= 10000000 && mass<=100000000){
-      document.querySelector('#b5').style.visibility = "visible";
-  }
-    else if (mass >= 100000000 && mass<=1000000000){
-      document.querySelector('#b6').style.visibility = "visible";
-  }
-    else if (mass >= 1000000000 && mass<=10000000000){
-      document.querySelector('#b7').style.visibility = "visible";
-  }
-    else if (mass >= 10000000000 && mass<=100000000000){
-      document.querySelector('#b8').style.visibility = "visible";
-  }
-    else if (mass >= 100000000000 && mass<=1000000000000){
-      document.querySelector('#b9').style.visibility = "visible";
-  }
-    else if (mass >= 1000000000000 && mass<=10000000000000){
-      document.querySelector('#b10').style.visibility = "visible";
-  }
-    else if (mass >= 10000000000000 && mass<=100000000000000){
-      document.querySelector('#b11').style.visibility = "visible";
-  }
-    else if (mass >= 100000000000000 && mass<=1000000000000000){
-      document.querySelector('#b12').style.visibility = "visible";
-    }
-  }
-  
+}
